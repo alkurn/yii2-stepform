2ed7427c00ccdeb175a01dacab17f47e9e72aec0
Ganesh <ganesh.alkurn@gmail.com>
2018-03-15 18:42:13 +0530

    first commit

---
+++ WizardWidgetAsset.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * @copyright Copyright &copy; A.F.Schuurman, 2015
+ * @package yii2-stepform
+ * @version 1.0.0
+ */
+namespace alkurn\stepform;
+
+use yii\web\AssetBundle;
+
+/**
+ * Asset bundle for Stepform Widget
+ *
+ * @author A.F.Schuurman <andre.schuurman+yii2-stepform@gmail.com>
+ * @since 1.0
+ */
+class stepformAsset extends AssetBundle
+{
+	public $sourcePath = '@alkurn/stepform';
+	public $depends = [
+		'yii\web\YiiAsset',
+		'yii\bootstrap\BootstrapPluginAsset'
+	];
+	public $css = [
+		'css/stepform.css',
+	];
+	public $js = [
+		'js/stepform.js'
+	];
+}
